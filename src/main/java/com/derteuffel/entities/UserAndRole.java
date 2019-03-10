f78d5d4e61ea91d7b9899d1e94350f9365b57c2b
derteuffel <derteuffel0@gmail.com>
2019-03-10 03:08:46 +0100

    setup user connexion in app and role auto attribution

---
+++ src/main/java/com/derteuffel/entities/UserAndRole.java
@@ -0,0 +1,51 @@
+package com.derteuffel.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by derteuffel on 09/03/2019.
+ */
+@Entity
+public class UserAndRole implements Serializable{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long userId;
+    private Long roleId;
+
+    public UserAndRole() {
+    }
+
+    public UserAndRole(Long userId, Long roleId) {
+        this.userId = userId;
+        this.roleId = roleId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+}
